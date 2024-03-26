509086e5e30506279f9ce6a78c9cb78b3629bd84
Jessica Pavani <jlepavani@gmail.com>
2024-03-26 06:48:27 -0300

    upload: simulation scripts

--- Simulation/Data-generating-III.R
+++ Simulation/Data-generating-III.R
@@ -1,5 +1,4 @@
-library(mvtnorm); library(sf); library(blockmatrix); library(Matrix)
-library(rgdal)
+library(mvtnorm); library(sf); library(sn)
 
 Output.Areas <- sf::read_sf(".", "RG2017_rgi"); remove(Output.Areas)
 Output.MG <- Output.Areas[217:286,]
@@ -20,32 +19,17 @@ X[,,2] = X[,,4] = rmvnorm(ntime, mean = rep(0, narea), sigma = diag(0.5, narea))
 W.nb <- spdep::poly2nb(Output.MG, row.names = 1:length(Output.MG)) 
 adj_mat <- spdep::nb2mat(W.nb, style = "B")
 
-# Spatial parameters
-alpha = c(0.5, 0.5); eta0_21 = 1; eta1_21 = 0.1
+# Global spatial dependence
+alpha = c(0.8, 0.6)
 
-# Distance matrix
-MG.coord = coordinates(Output.MG)
-MG.latrange = round(quantile(MG.coord[,2], c(0.25, 0.75)))
-MG.albersproj = mapproj::mapproject(MG.coord[,1], MG.coord[,2], projection = "albers", param = MG.latrange)
-projmat = cbind(MG.albersproj$x, MG.albersproj$y)
-
-perm = order(MG.albersproj$x + MG.albersproj$y)
-adj_mat = adj_mat[perm, perm]
-projmat = projmat[perm,]
-
-dmat = as.matrix(dist(projmat))
-dmat = dmat/mean(dmat[which(adj_mat==1)])
-
-# Spatial covariance matrix
-G1 = alpha[1]^dmat; G2 = alpha[2]^dmat
-A = diag(eta0_21, narea) + eta1_21 * adj_mat
-L = as.matrix(blockmatrix(names = c("I1","A","0","I2"), I1 = diag((ndisease-1)*narea),
-                          A = A, I2 = diag(narea), dim=c(2,2)))
-G = as.matrix(bdiag(G1, G2))
-V = L %*% G %*% t(L)
+# Spatial covariance matrix: Q(adj_mat, alpha)
+set.seed(1); Q1 <- solve(diag(apply(adj_mat, 2, sum)) - alpha[1] * adj_mat)
+set.seed(2); Q2 <- solve(diag(apply(adj_mat, 2, sum)) - alpha[2] * adj_mat)
 
 # Spatial random effects
-phi = c(rmvnorm(1, mean = rep(0, ndisease*narea), sigma = 0.0001*solve(V)))
+phi1 <- mvtnorm::rmvnorm(1, mean = rep(0, narea), sigma = Q1)
+phi2 <- mvtnorm::rmvnorm(1, mean = rep(0, narea), sigma = Q2)
+phi = c(phi1, phi2)
 
 # Cluster structure - 1 cluster
 partition = rep(1, narea)
@@ -53,13 +37,11 @@ ncl = length(unique(partition))
 
 nrho = qq = 2
 rho_star = matrix(c(1.6, -0.7, 1.6, -0.7), ndisease*nrho, ncl)
-var_dta_star = 0.001
+var_dta_star = 0.1
 
 rho = matrix(NA, ndisease*nrho, narea)
-var_dta = rep(NA, narea)
 for(j in 1:ncl){
   rho[,partition == j] = rho_star[,j]
-  var_dta[partition == j] = var_dta_star[j]
 }
 
 # Cluster structure - 2 clusters
@@ -69,13 +51,11 @@ ncl = length(unique(partition))
 
 nrho = qq = 2
 rho_star = matrix(c(1.6, -0.7, 1.6, -0.7, 0.9, -0.1, 0.9, -0.1), ndisease*nrho, ncl)
-var_dta_star = c(0.001, 0.001)
+var_dta_star = 0.1
 
 rho = matrix(NA, ndisease*nrho, narea)
-var_dta = rep(NA, narea)
 for(j in 1:ncl){
   rho[,partition == j] = rho_star[,j]
-  var_dta[partition == j] = var_dta_star[j]
 }
 
 # Cluster structure - 3 clusters
@@ -86,40 +66,33 @@ ncl = length(unique(partition))
 nrho = qq = 2
 rho_star = matrix(c(1.6, -0.7, 1.6, -0.7, 0.3, 0.1, 0.3, 0.1,
                     0.9, -0.1, 0.9, -0.1), ndisease*nrho, ncl)
-var_dta_star = c(0.001, 0.001, 0.001)
+var_dta_star = 0.1
 
 rho = matrix(NA, ndisease*nrho, narea)
-var_dta = rep(NA, narea)
 for(j in 1:ncl){
   rho[,partition == j] = rho_star[,j]
-  var_dta[partition == j] = var_dta_star[j]
 }
 
+var_dta = matrix(c(var_dta_star, 0.05, 0.05, var_dta_star), 2, 2)
+  
 # Response variable
 dta = array(NA, dim = c(narea, ntime, ndisease))
 for(i in 1:narea){
   
   # tt = 1
-  mean1 = phi[i] + X[i,1,1:2] %*% beta[1:2]
-  dta[i,1,1] = rnorm(1, mean = mean1, sd = sqrt(var_dta[i]))
-  
-  mean2 = phi[i+narea] + X[i,1,3:4] %*% beta[3:4]
-  dta[i,1,2] = rnorm(1, mean = mean2, sd = sqrt(var_dta[i]))
+  m = c(phi[i] + X[i,1,1:2] %*% beta[1:2], phi[i+narea] + X[i,1,3:4] %*% beta[3:4])
+  dta[i,1,] = sn::rmsn(1, xi = m, Omega = var_dta, alpha = c(1,10))
   
   # tt = 2
-  mean1 = phi[i] + X[i,2,1:2] %*% beta[1:2] + dta[i,1,1] * rho[1,i]
-  dta[i,2,1] = rnorm(1, mean = mean1, sd = sqrt(var_dta[i]))
-  
-  mean2 = phi[i+narea] + X[i,2,3:4] %*% beta[3:4] + dta[i,1,2] * rho[3,i]
-  dta[i,2,2] = rnorm(1, mean = mean2, sd = sqrt(var_dta[i]))
+  m = c(phi[i] + X[i,2,1:2] %*% beta[1:2] + dta[i,1,1] * rho[1,i],
+        phi[i+narea] + X[i,2,3:4] %*% beta[3:4] + dta[i,1,2] * rho[3,i])
+  dta[i,2,] = sn::rmsn(1, xi = m, Omega = var_dta, alpha = c(1,10))
   
   # tt > qq
-  for(tt in (qq+1):(ntime+npred)){
-    mean1 = phi[i] + X[i,tt,1:2] %*% beta[1:2] + dta[i,tt-(1:2),1] %*% rho[1:2,i]
-    dta[i,tt,1] = rnorm(1, mean = mean1, sd = sqrt(var_dta[i]))
-    
-    mean2 = phi[i+narea] + X[i,tt,3:4] %*% beta[3:4] + dta[i,tt-(1:2),2] %*% rho[3:4,i]
-    dta[i,tt,2] = rnorm(1, mean = mean2, sd = sqrt(var_dta[i]))
+  for(tt in (qq+1):ntime){
+    m = c(phi[i] + X[i,tt,1:2] %*% beta[1:2] + dta[i,tt-(1:2),1] %*% rho[1:2,i],
+          phi[i+narea] + X[i,tt,3:4] %*% beta[3:4] + dta[i,tt-(1:2),2] %*% rho[3:4,i])
+    dta[i,tt,] = sn::rmsn(1, xi = m, Omega = var_dta, alpha = c(1,10))
   }
 }
 
